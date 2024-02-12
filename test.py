08124693a346f7dca2f8c4597ed06d8f8c657a27
Bill <admin@wdufrin.altostrat.com>
2024-02-12 14:27:49 +0000

    Changed to a Chatbot!

--- test.py
+++ test.py
@@ -4,12 +4,13 @@ import vertexai
 from vertexai.language_models import TextGenerationModel
 from vertexai.language_models import GroundingSource
 from google.cloud import aiplatform
+from vertexai.language_models import ChatModel, InputOutputTextPair
 
 PROJECT_ID = "ancient-sandbox-322523"  
 LOCATION = "us-central1" 
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
-model = TextGenerationModel.from_pretrained("text-bison")
+model = ChatModel.from_pretrained("chat-bison")
 
 def create_session():
     chat_model = vertexai.language_models.ChatModel.from_pretrained("chat-bison@001")
@@ -18,11 +19,12 @@ def create_session():
 
 def response(chat, message):
     parameters = {
-        "temperature": 0.2,
-        "max_output_tokens": 256,
-        "top_p": 0.8,
-        "top_k": 40
+        "candidate_count": 1,
+        "temperature": 0,
+        "max_output_tokens": 1024,
+        "top_p": 1
     }
+
     grounding_source=GroundingSource.VertexAISearch(data_store_id="sunlit2_1704293809107", location="global", project="180054373655")
     result = chat.send_message(message, **parameters,grounding_source=grounding_source)
     return result.text
@@ -32,7 +34,7 @@ def run_chat():
     print(f"Chat Session created")
     
     while True:
-        user_input = input("You: ")
+        user_input = input("Input: ")
         if user_input.lower() in ['exit', 'quit']:
             break
         
