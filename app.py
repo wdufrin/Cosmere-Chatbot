dce3419665158ac59e27d85d8f2b49059dd0559c
William Dufrin <wdufrin@google.com>
2024-04-29 15:34:09 +0000

    more 1.5 modifications.

--- app.py
+++ app.py
@@ -1,46 +1,59 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
+from typing import Optional
+import base64
 import vertexai
-from vertexai.language_models import ChatModel
-from vertexai.language_models import GroundingSource
-import os
-from vertexai.language_models import ChatModel, InputOutputTextPair
-
+from vertexai.generative_models import GenerativeModel, Part, Tool
+import vertexai.preview.generative_models as generative_models
 
 app = Flask(__name__)
 PROJECT_ID = "ancient-sandbox-322523"  
 LOCATION = "us-central1"  
 
-vertexai.init(project=PROJECT_ID, location=LOCATION)
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": .5,
+    "top_p": 0.95,
+}
 
-def create_session():
-    chat_model = vertexai.language_models.ChatModel.from_pretrained("chat-bison@001")
-    chat = chat_model.start_chat()
-    return chat
+safety_settings = {
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+}
 
-def response(chat, message):
-    parameters = {
-        "temperature": 0,
-        "max_output_tokens": 1024,
-        "top_p": 1
-    }
-    grounding_source=GroundingSource.VertexAISearch(data_store_id="sunlit2_1704293809107", location="global", project="180054373655")
-    result = chat.send_message(message, **parameters,grounding_source=grounding_source)
-    return result.text
+def create_model():
+    vertexai.init(project=PROJECT_ID, location=LOCATION)
+    tools = [
+        Tool.from_retrieval(
+            retrieval=generative_models.grounding.Retrieval(
+                source=generative_models.grounding.VertexAISearch(datastore="projects/180054373655/locations/global/collections/default_collection/dataStores/sunlit2_1704293809107"),
+                disable_attribution=False,
+            )
+        ),
+    ]
+    model = GenerativeModel(
+        "gemini-1.5-pro-preview-0409",
+        tools=tools,
+        system_instruction=["""You are a fun and whitty companion.  You always tell the truth and are not afraid to say you do not know the answer to something."""]
+    )
+    return model
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+model = create_model()
 
-@app.route('/palm2', methods=['GET', 'POST'])
-def vertex_palm():
-    user_input = ""
-    if request.method == 'GET':
-        user_input = request.args.get('user_input')
-    else:
-        user_input = request.form['user_input']
-    chat_model = create_session()
-    content = response(chat_model,user_input)
-    return jsonify(content=content)
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json['message']
+    content = multiturn_generate_content(model, user_input)
+    return jsonify({'message': content.text})
+
+def multiturn_generate_content(model: GenerativeModel, user_input: str) -> str:
+  chat = model.start_chat()
+  return chat.send_message(
+      [user_input],
+      generation_config=generation_config,
+      safety_settings=safety_settings
+  )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080, host='0.0.0.0')
+    app.run()
--- test4.py
+++ test4.py
@@ -1,43 +1,37 @@
 from typing import Optional
-
+import base64
 import vertexai
-from vertexai.preview.generative_models import (
-    GenerationResponse,
-    GenerativeModel,
-    grounding,
-    Tool,
-)
+from vertexai.generative_models import GenerativeModel, Part, Tool
+import vertexai.preview.generative_models as generative_models
 
-PROJECT_ID = "ancient-sandbox-322523"  
-LOCATION = "us-central1" 
-data_store_path = "projects/ancient-sandbox-322523/locations/us-central1/collections/default_collection/dataStores/sunlit2_1704293809107"
-model = GenerativeModel(model_name="gemini-1.0-pro")
 
-def generate_text_with_grounding(
-    PROJECT_ID: str, LOCATION: str, data_store_path: str, message: str
-) -> GenerationResponse:
-    # Initialize Vertex AI
-    vertexai.init(project=PROJECT_ID, location=LOCATION)
-    data_store_path = "projects/ancient-sandbox-322523/locations/us-central1/collections/default_collection/dataStores/sunlit2_1704293809107"
+def multiturn_generate_content(model: GenerativeModel, user_input: str) -> str:
+  chat = model.start_chat()
+  return chat.send_message(
+      [user_input],
+      generation_config=generation_config,
+      safety_settings=safety_settings
+  )
 
-    # Create Tool for grounding
-    tool = Tool.from_retrieval(
-        grounding.Retrieval(grounding.VertexAISearch(datastore=data_store_path))
-    )
 
- #   print(f"message:", message)
- #   print(f"tool:", tool)
-    
-    
-    response = model.generate_content(message)
-    #response = model.generate_content(message, tools=[tool])
-    return response
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": .5,
+    "top_p": 0.95,
+}
 
+safety_settings = {
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+}
 
 def create_session():
-    model = GenerativeModel(model_name="gemini-1.0-pro")
+    chat_model = model
     chat = model.start_chat()
     return chat
+
 
 def run_chat():
     chat_model = create_session()
@@ -48,23 +42,22 @@ def run_chat():
         if user_input.lower() in ['exit', 'quit']:
             break
         
-        content = response(chat_model, user_input)
-        print(f"AI: {content}")
-
-def response(chat, message):
-    parameters = {
-        "candidate_count": 1,
-        "temperature": 0,
-        "max_output_tokens": 2048,
-        "top_p": 1
-    }
-
-    result = generate_text_with_grounding(PROJECT_ID,LOCATION,data_store_path,message)
-    print("RESULTS: ", result.text)
-    return result.text
-
+        content = multiturn_generate_content(model, user_input)
+        print(f"AI: {content.text}")
 
 if __name__ == '__main__':
+    vertexai.init(project="ancient-sandbox-322523", location="us-central1")
+    tools = [
+        Tool.from_retrieval(
+            retrieval=generative_models.grounding.Retrieval(
+                source=generative_models.grounding.VertexAISearch(datastore="projects/180054373655/locations/global/collections/default_collection/dataStores/sunlit2_1704293809107"),
+                disable_attribution=False,
+            )
+        ),
+    ]
+    model = GenerativeModel(
+        "gemini-1.5-pro-preview-0409",
+        tools=tools,
+        system_instruction=["""You are a fun and whitty companion.  You always tell the truth and are not afraid to say you do not know the answer to something."""]
+    )
     run_chat()
-
-
